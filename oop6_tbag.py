f510a7cca0bbf7b1c35ebea0464735da3fa46285
shadoworay <97796821+shadoworay@users.noreply.github.com>
2022-05-03 01:20:36 +0530

    test commit

--- oop6_tbag.py
+++ oop6_tbag.py
@@ -6,6 +6,6 @@ dining_hall = Room("Dining Hall")
 
 kitchen.set_description("A dingy, poorly lit room, teeming with the smell of rotting waste and burning coal")
 ballroom.set_description("Long forgotten, and untouched for who knows how long, the crumbling walls of this room")
-
+dining_hall.set_description("A table, so long that it may as well fit a 100 people, is the centre of attraction here")
 
 kitchen.describe()
