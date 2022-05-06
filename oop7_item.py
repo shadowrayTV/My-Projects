44d254ec218f7f451f22efd1f2592de13dbee697
shadoworay <97796821+shadoworay@users.noreply.github.com>
2022-05-06 06:15:11 +0530

    End of Week 2

--- oop7_item.py
+++ oop7_item.py
@@ -2,3 +2,14 @@ class Item:
     def __init__(self):
         self.name = None
         self.description = None
+
+    def set_name(self, item_name):
+        self.name = item_name
+
+    def get_name(self):
+        return self.name
+
+    def set_description(self, item_desc):
+        self.description = item_desc
+
+
